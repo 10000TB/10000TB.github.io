9336b4ec0087bda092b20dc1d937ab7a1a60ed3a
Xuehao(David)  Hu <xuehao.hu@gmail.com>
2018-03-24 01:52:09 -0700

    removed a junk file.

--- _posts/test.py
+++
@@ -1,8 +0,0 @@
-
-
-This is an example text to show how to cancel search highlight.
-
-This is an example text to show how to cancel search highlight.
-
-This is an example text to show how to cancel search highlight.
-
